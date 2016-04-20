c96bc445352b5381628fa5369b0baaa8aab48374
ShashankEzetap <shashank.b@ezetap.com>
2016-04-20 14:29:18 +0530

    Moving the source files

--- source/src/com/ezetap/sdk/HttpResponseHandler.java
+++
@@ -1,9 +0,0 @@
-package com.ezetap.sdk;
-
-import org.json.JSONObject;
-
-public interface HttpResponseHandler {
-
-	public void handleResponse(JSONObject response, int requestCode);
-	public void handleError(Exception e, String msg, int requestCode);
-}
